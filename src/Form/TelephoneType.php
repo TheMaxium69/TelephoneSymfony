0dd1d02695adebd5bf781852cfc10fc6d229fdc5
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-07-16 13:25:42 +0200

    Create TelephoneType.php

---
+++ src/Form/TelephoneType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Telephone;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TelephoneType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('modelName')
+            ->add('img')
+            ->add('price')
+            ->add('description')
+            ->add('stockage')
+            ->add('createdDate')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Telephone::class,
+        ]);
+    }
+}
