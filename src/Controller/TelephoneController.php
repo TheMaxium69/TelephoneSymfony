7aeb61468b95416023c1ef0e2aa232deaa7e33ef
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-07-16 11:40:40 +0200

    Telephone index, show, del

---
+++ src/Controller/TelephoneController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Telephone;
+use App\Repository\TelephoneRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TelephoneController extends AbstractController
+{
+    /**
+     * @Route("/telephone", name="telephoneIndex")
+     */
+    public function index(TelephoneRepository $repository): Response
+    {
+
+        $tels = $repository->findAll();
+
+        return $this->render('telephone/index.html.twig', [
+            'telephones' => $tels,
+        ]);
+    }
+
+    /**
+     * @Route("/telephone/{id}", name="telephoneShow")
+     */
+    public function show(Telephone $telephone): Response
+    {
+        return $this->render('telephone/show.html.twig', [
+            'telephone' => $telephone,
+        ]);
+    }
+
+
+    /**
+     * @Route("/telephone/{id}/del", name="telephoneDel")
+     */
+    public function del(Telephone $telephone, EntityManagerInterface $manager) : Response
+    {
+
+        $manager->remove($telephone);
+        $manager->flush();
+
+        return $this->redirectToRoute('telephoneIndex');
+    }
+
+}
---
+++ src/Entity/Telephone.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TelephoneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TelephoneRepository::class)
+ */
+class Telephone
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $modelName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $img;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $stockage;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getModelName(): ?string
+    {
+        return $this->modelName;
+    }
+
+    public function setModelName(string $modelName): self
+    {
+        $this->modelName = $modelName;
+
+        return $this;
+    }
+
+    public function getImg(): ?string
+    {
+        return $this->img;
+    }
+
+    public function setImg(string $img): self
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getStockage(): ?int
+    {
+        return $this->stockage;
+    }
+
+    public function setStockage(int $stockage): self
+    {
+        $this->stockage = $stockage;
+
+        return $this;
+    }
+
+    public function getCreatedDate(): ?\DateTimeInterface
+    {
+        return $this->createdDate;
+    }
+
+    public function setCreatedDate(\DateTimeInterface $createdDate): self
+    {
+        $this->createdDate = $createdDate;
+
+        return $this;
+    }
+}
