5281a16243053f522b0e3d6ceb7ac436c49002a9
TheMaximeSan <mxm.tournier@gmail.com>
2021-07-21 17:57:17 +0200

    Create Version20210721114345.php

---
+++ migrations/Version20210721114345.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210721114345 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE constructeur (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, img_logo VARCHAR(255) NOT NULL, origin VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, address LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE telephone (id INT AUTO_INCREMENT NOT NULL, constructeur_id INT NOT NULL, model_name VARCHAR(255) NOT NULL, img VARCHAR(255) NOT NULL, price INT NOT NULL, description LONGTEXT NOT NULL, stockage INT NOT NULL, created_date DATETIME NOT NULL, INDEX IDX_450FF0108815B605 (constructeur_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE telephone ADD CONSTRAINT FK_450FF0108815B605 FOREIGN KEY (constructeur_id) REFERENCES constructeur (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE telephone DROP FOREIGN KEY FK_450FF0108815B605');
+        $this->addSql('DROP TABLE constructeur');
+        $this->addSql('DROP TABLE telephone');
+    }
+}
